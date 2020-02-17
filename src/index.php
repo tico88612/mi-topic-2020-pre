55f4008a92ac1669e41315748a38a07a3826f687
tico88612 <tico88612@yahoo.com.tw>
2020-02-17 22:02:37 +0800

    first html php

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<?php
+include_once('head.php');
+?>
+
+    <h1 class="title">
+    Hello World
+    </h1>
+    <p class="subtitle">
+    My first website with <strong>Bulma</strong>!
+    </p>
+    
+
+    <script>
+    Swal.fire(
+        'Good job!',
+        'You clicked the button!',
+        'success'
+    );
+    </script>
+
+<?php
+include_once('footer.php');
+?>
