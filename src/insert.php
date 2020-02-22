514df2cc6e8d288cd8907fd60353e23b86a6f034
tico88612 <tico88612@yahoo.com.tw>
2020-02-22 17:54:55 +0800

    add inserturi

---
+++ src/insert.php
@@ -0,0 +1,70 @@
+<?php
+include_once('head.php');
+?>
+<!-- <form action="" method="post"> -->
+    <div class="field">
+    <label for="inputURi" class="label">網址</label>
+    <div class="control">
+        <input name="inputURi" id="inputURi" class="input" type="text" placeholder="E.g. https://...">
+    </div>
+    <!-- <p class="help">This is a help text</p> -->
+    </div>
+    <div class="field is-grouped">
+    <div class="control">
+        <button class="button is-link" onclick="submitPOST();">新增</button>
+    </div>
+    <div class="control">
+        <button class="button is-link is-light">Cancel</button>
+    </div>
+    </div>
+<!-- </form> -->
+
+    <script>
+    
+    function submitPOST(){
+        Swal.showLoading();
+        let s = document.getElementById('inputURi').value;
+        let data = {uri: s};
+        let response = fetch(`/test.php`, {
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        })
+        .catch(error => {
+            Swal.showValidationMessage(
+            `Request failed: ${error}`
+            )
+        })
+        .then(responseJson => {
+            Swal.hideLoading();
+            console.log(responseJson);
+            if(responseJson["HTTP_STATUS_CODE"] == 200){
+                Swal.fire(
+                'Oh Geez!',
+                'Your URL has been added.',
+                'success'
+                );
+            }
+            else{
+                Swal.fire(
+                'HTTP Code:' + responseJson["HTTP_STATUS_CODE"],
+                responseJson["Message"],
+                'error'
+                );
+            }
+        })
+        
+    }
+    </script>
+
+<?php
+include_once 'footer.php';
+?>
---
+++ src/inserturi.php
@@ -0,0 +1,106 @@
+<?php
+    ini_set('display_errors','1');
+    error_reporting(E_ALL);
+    include_once('connectdb.php');
+    function getURI(){
+        $request = json_decode(file_get_contents('php://input'), true);
+        if(!isset($request['uri'])){
+            return false;
+        }
+        return $request['uri'];
+    }
+    function validateURI($URI){
+        $regex = "/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/m";
+        if(preg_match($regex, $URI)){
+            return true;
+        }
+        return false;
+    }
+    function queryURIisInDatabase($URI, $dbh){
+        $sql = "SELECT * FROM `web` WHERE `webURI` = ?";
+        $sth = $dbh->prepare($sql);
+        $sth->execute(array($URI));
+        $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+        if(count($rows) == 0){
+            return false;
+        }
+        return true;
+    }
+    function getContext($URI){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $URI);
+        //永遠抓最新
+        $request_headers = array(
+            "Cache-Control: no-cache",
+            "Pragma: no-cache",
+            "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36"
+        );
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
+        //等待時間
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 4);
+        // 設定referer
+        curl_setopt($ch, CURLOPT_REFERER, 'https://www.google.com');
+        curl_setopt($ch, CURLOPT_URL, $URI);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+    function goInDatabase($URI, $dbh){
+        try {
+            $sql = "INSERT INTO `web`(`webURI`, `webContext`) VALUES (?, ?)";
+            $context = getContext($URI);
+            $sth = $dbh->prepare($sql);
+            $sth->execute(array($URI, $context));
+            //success
+            $return = array(true, "Success!");
+            return $return;
+        }
+        catch (PDOException $e) {
+            //error
+            $return = array(false, "FAIL! ". $e);
+            return $return;
+        }
+    }
+
+
+
+    $URI = getURI();
+    $returnData = array();
+    if($URI == false){
+        $returnData["HTTP_STATUS_CODE"] = 400;
+        $returnData["Message"] = "Bad Request";
+        $returnString = json_encode($returnData);
+        echo $returnString;
+        die();
+    }
+    if(validateURI($URI) === false){
+        $returnData["HTTP_STATUS_CODE"] = 400;
+        $returnData["Message"] = "Bad Request: URI not match.";
+        $returnString = json_encode($returnData);
+        echo $returnString;
+        die();
+    }
+    if(queryURIisInDatabase($URI, $dbh)){
+        $returnData["HTTP_STATUS_CODE"] = 400;
+        $returnData["Message"] = "Bad Request: URI is in Database.";
+        $returnString = json_encode($returnData);
+        echo $returnString;
+        die();
+    }
+    $getResult = goInDatabase($URI, $dbh);
+    if($getResult[0] == true){
+        $returnData["HTTP_STATUS_CODE"] = 200;
+        $returnData["Message"] = "Success";
+        $returnString = json_encode($returnData);
+    }
+    else{
+        $returnData["HTTP_STATUS_CODE"] = 400;
+        $returnData["Message"] = $getResult[1];
+        $returnString = json_encode($returnData);
+    }
+    echo $returnString;
+
+
+?>
