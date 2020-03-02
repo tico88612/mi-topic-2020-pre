68cce1f27aa37de4d59ec462e42af7f8a0b3a317
tico88612 <tico88612@yahoo.com.tw>
2020-03-02 22:27:40 +0800

    search page complete

--- src/index.php
+++ src/index.php
@@ -1,15 +1,22 @@
 <?php
 include_once('head.php');
 ?>
-
-    <h1 class="title">
-    Hello World
-    </h1>
-    <p class="subtitle">
-    My first website with <strong>Bulma</strong>!
-    </p>
-    
-
+<form action="query.php" method="get">
+    <div class="field">
+    <label for="query" class="label"></label>
+    <div class="control">
+        <input name="query" id="query" class="input" type="text" placeholder="輸入關鍵字">
+    </div>
+    </div>
+    <div class="field is-grouped">
+    <div class="control">
+        <button class="button is-link" type="submit">搜尋</button>
+    </div>
+    <div class="control">
+        <button class="button is-link is-light" type="reset">重置</button>
+    </div>
+    </div>
+</form>
     
 
 <?php
