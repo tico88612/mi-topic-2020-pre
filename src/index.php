045c2ac2a47bc96d2022393a62bcc0b8363cbe38
tico88612 <tico88612@yahoo.com.tw>
2020-03-21 21:43:44 +0800

    Format code & add some js in index & query

--- src/index.php
+++ src/index.php
@@ -1,24 +1,37 @@
 <?php
 include_once('head.php');
 ?>
-<form action="query.php" method="get">
+<form id="myForm" action="query.php" method="get">
     <div class="field">
-    <label for="query" class="label"></label>
-    <div class="control">
-        <input name="query" id="query" class="input" type="text" placeholder="輸入關鍵字">
-    </div>
+        <label for="query" class="label"></label>
+        <div class="control">
+            <input name="query" id="query" class="input" type="text" placeholder="輸入關鍵字..." onfocus="this.placeholder=''" onblur="this.placeholder='輸入關鍵字...'">
+        </div>
     </div>
     <div class="field is-grouped">
-    <div class="control">
-        <button class="button is-link" type="submit">搜尋</button>
-    </div>
-    <div class="control">
-        <button class="button is-link is-light" type="reset">重置</button>
-    </div>
+        <div class="control">
+            <input type="button" class="button is-link" value="搜尋" onclick="submitForm()">
+        </div>
+        <div class="control">
+            <input class="button is-link is-light" value="重置" type="reset">
+        </div>
     </div>
 </form>
-    
-
+<script>
+    function submitForm() {
+        var str = document.getElementById('query').value;
+        if (str.trim() == '') {
+            Swal.fire(
+                '錯誤',
+                '搜尋字串不為空白！',
+                'error'
+            );
+        } else {
+            document.getElementById('query').value = str.trim();
+            document.getElementById("myForm").submit()
+        }
+    }
+</script>
 <?php
 include_once('footer.php');
 ?>
--- src/query.php
+++ src/query.php
@@ -2,16 +2,17 @@
 include_once('head.php');
 include_once('connectdb.php');
 
-function queryDB($query, $dbh){
+function queryDB($query, $dbh)
+{
     $sql = "SELECT * FROM `web` WHERE `webTitle` LIKE ? OR `webContext` LIKE ?";
     $sth = $dbh->prepare($sql);
-    $query = "%".$query."%";
+    $query = "%" . $query . "%";
     $sth->execute(array($query, $query));
     $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
     return $rows;
 }
 
-if(!isset($_GET['query'])){
+if (!isset($_GET['query'])) {
     header("Refresh: 0;url=/");
     die();
 }
@@ -20,43 +21,59 @@ $queryResult = queryDB($_GET['query'], $dbh);
 $queryLength = count($queryResult);
 
 ?>
-<form action="query.php" method="get">
+<form id="myForm" action="query.php" method="get">
     <div class="field">
-    <label for="query" class="label"></label>
-    <div class="control">
-        <input name="query" id="query" class="input" type="text" placeholder="輸入關鍵字" value="<?php echo htmlspecialchars($_GET['query'], ENT_QUOTES, 'UTF-8'); ?>">
-    </div>
+        <label for="query" class="label"></label>
+        <div class="control">
+            <input name="query" id="query" class="input" type="text" placeholder="輸入關鍵字..." onfocus="this.placeholder=''" onblur="this.placeholder='輸入關鍵字...'" value="<?php echo htmlspecialchars($_GET['query'], ENT_QUOTES, 'UTF-8'); ?>">
+        </div>
     </div>
     <div class="field is-grouped">
-    <div class="control">
-        <button class="button is-link" type="submit">搜尋</button>
-    </div>
-    <div class="control">
-        <button class="button is-link is-light" type="reset">重置</button>
-    </div>
+        <div class="control">
+            <input type="button" class="button is-link" value="搜尋" onclick="submitForm()">
+        </div>
+        <div class="control">
+            <input class="button is-link is-light" value="重置" type="reset">
+        </div>
     </div>
 </form>
+<script>
+    function submitForm() {
+        var str = document.getElementById('query').value;
+        if (str.trim() == '') {
+            Swal.fire(
+                '錯誤',
+                '搜尋字串不為空白！',
+                'error'
+            );
+        } else {
+            document.getElementById('query').value = str.trim();
+            document.getElementById("myForm").submit()
+        }
+    }
+</script>
 
 <p class="help">有 <?php echo $queryLength; ?> 項結果</p>
 
 <br>
 
 <style>
-    .urltitle{
+    .urltitle {
         font-size: 28px;
         font-weight: bold;
         margin-bottom: 0;
     }
-    .urltext{
+
+    .urltext {
         font-size: 18px;
         color: #63bf68;
     }
 </style>
 
-<?php 
-for($i = 0; $i < $queryLength; $i++){
-    echo '<h3 class="urltitle"><a href="'.$queryResult[$i]['webURI'].'">'.$queryResult[$i]['webTitle'].'</a></h2>';
-    echo '<p class="urltext">'.$queryResult[$i]['webURI'].'</p><br>';
+<?php
+for ($i = 0; $i < $queryLength; $i++) {
+    echo '<h3 class="urltitle"><a href="' . $queryResult[$i]['webURI'] . '">' . $queryResult[$i]['webTitle'] . '</a></h2>';
+    echo '<p class="urltext">' . $queryResult[$i]['webURI'] . '</p><br>';
 }
 ?>
 
